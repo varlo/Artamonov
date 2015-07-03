ab0bd7ae9b73389280425056c8e8f5a1c467dc32
varlo <varlo@ukr.net>
2015-07-03 11:48:12 +0300

    Добавил ссылки на Functions.aspx.

--- stroytehnadzor/Functions.aspx.designer.cs
+++ stroytehnadzor/Functions.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace Stroytehnadzor {
     
     
     public partial class Functions {
+        
+        /// <summary>
+        /// lbInstructionPdf control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbInstructionPdf;
     }
 }
