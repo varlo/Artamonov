158150bad4aa909932557927d639953fada00467
varlo <varlo@ukr.net>
2015-07-06 21:45:41 +0300

    Добавил 4-ю страницу.

---
+++ stroytehnadzor/Flaw4.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
