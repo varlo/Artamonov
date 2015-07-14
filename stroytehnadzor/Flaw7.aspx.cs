2624f25f6b0cdb1ebbcf90193ad96b411b3f8d1b
varlo <varlo@ukr.net>
2015-07-14 09:55:21 +0300

    Добавил 7 новость по браку.

---
+++ stroytehnadzor/Flaw7.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
