4b8b2fb0df4eb01ac7e25f2850c479738ba1ccbd
varlo <varlo@ukr.net>
2015-07-15 16:29:48 +0300

    Добавил 8-ю статью.

---
+++ stroytehnadzor/Flaw8.aspx.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
