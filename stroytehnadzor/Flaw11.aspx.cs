965d2cacbfe677f5d84840c4c9f8537b5494cf5f
varlo <varlo@ukr.net>
2015-07-21 08:10:25 +0300

    Добавил 11 статью по браку.

---
+++ stroytehnadzor/Flaw11.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw11 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("B7219552-7C9A-4F63-A6A5-077CF57A17A5"); }
+        }
+    }
+}
