4a3ce9223e898aaf0e3ebd8546f44a2e2c6feec8
varlo <varlo@ukr.net>
2015-07-22 15:26:23 +0300

    Добавил статью по браку №12.

---
+++ stroytehnadzor/Flaw12.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw12 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("518F8F47-7295-4823-85D2-C53C307FBEA2"); }
+        }
+    }
+}
