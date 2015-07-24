2cca7621634eddb0ba89a49844a8c1c681f105cf
varlo <varlo@ukr.net>
2015-07-24 17:09:24 +0300

    Добавил статью по браку №13.

---
+++ stroytehnadzor/Flaw13.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw13 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("32E0FDA3-F422-43C2-ACC0-04AC371ACAF7"); }
+        }
+    }
+}
