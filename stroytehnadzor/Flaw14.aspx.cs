cfda3902a1305daa3276895f0c2ce2fccc5b5452
varlo <varlo@ukr.net>
2015-07-27 16:15:50 +0300

    Добавил 14-ю статью по браку.

---
+++ stroytehnadzor/Flaw14.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw14 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("70209A38-FC14-48EB-85D6-D331AEFD1BB1"); }
+        }
+    }
+}
