06efb86885516bfafc86198cf43be26f2987e4ca
varlo <varlo@ukr.net>
2015-07-31 20:10:52 +0300

    Добавил 16-ю статью по браку.

---
+++ stroytehnadzor/Flaw16.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw16 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("451CBEF5-850B-4A8A-92F2-B579C77E05C5"); }
+        }
+    }
+}
