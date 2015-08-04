f0e8db4791493adca0698ef6b1a2cfbfc309c5f0
varlo <varlo@ukr.net>
2015-08-04 22:24:29 +0300

    Добавил 17-ю статью по браку.

---
+++ stroytehnadzor/Flaw17.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw17 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("66BA1703-F5CB-4BDE-85E4-5CBFE8AA5D89"); }
+        }
+    }
+}
