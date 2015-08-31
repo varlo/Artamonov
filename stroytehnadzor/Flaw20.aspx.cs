36d3652a6cd59c4f48f402b6e48dd76bff078416
varlo <varlo@ukr.net>
2015-08-31 16:11:00 +0300

    Добавил 20-ю статью.

---
+++ stroytehnadzor/Flaw20.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Stroytehnadzor
+{
+    public partial class Flaw20 : BasePage
+    {
+        public override Guid PageId
+        {
+            get { return new Guid("73217D76-CBE8-4AFB-9ABA-06B0A8488F83"); }
+        }
+    }
+}
